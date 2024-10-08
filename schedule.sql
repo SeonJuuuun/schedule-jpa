9a16adb6feaccdff381cdad9be283234e6a485b3
yuseonjun <seonjun0906@gmail.com>
2024-10-08 15:45:11 +0900

    [Docs] Level2 schedule.sql 수정

--- schedule.sql
+++ schedule.sql
@@ -1,5 +1,3 @@
-DROP TABLE IF EXISTS SCHEDULE;
-
 CREATE TABLE `schedule`
 (
     `id`         BIGINT      NOT NULL COMMENT 'Auto_Increment',
@@ -8,4 +6,14 @@ CREATE TABLE `schedule`
     `content`    VARCHAR(50) NOT NULL,
     `created_at` DATETIME    NOT NULL,
     `updated_at` DATETIME    NOT NULL
+);
+
+CREATE TABLE `comment`
+(
+    `id`          BIGINT       NOT NULL COMMENT 'Auto_Increment',
+    `content`     VARCHAR(250) NOT NULL,
+    `schedule_id` BIGINT       NOT NULL COMMENT 'FK',
+    `user_id`     BIGINT       NOT NULL COMMENT 'FK',
+    `created_At`  DATETIME     NOT NULL,
+    `updated_at`  DATETIME     NOT NULL
 );
