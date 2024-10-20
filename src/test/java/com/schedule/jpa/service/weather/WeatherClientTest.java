4cbc1a5b41e1e4a0d104ec8ded046cc4174a51d2
yuseonjun <seonjun0906@gmail.com>
2024-10-20 12:59:08 +0900

    [Test] WeatherClient 테스트 코드 수정

--- src/test/java/com/schedule/jpa/service/weather/WeatherClientTest.java
+++ src/test/java/com/schedule/jpa/service/weather/WeatherClientTest.java
@@ -1,30 +1,22 @@
 package com.schedule.jpa.service.weather;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import com.schedule.jpa.infra.client.weather.WeatherClient;
 import com.schedule.jpa.infra.client.weather.dto.WeatherResponse;
-import io.netty.channel.ChannelOption;
-import io.netty.handler.timeout.ReadTimeoutHandler;
-import io.netty.handler.timeout.WriteTimeoutHandler;
-import java.time.Duration;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClient.RequestHeadersSpec;
+import org.springframework.web.reactive.function.client.WebClient.RequestHeadersUriSpec;
+import org.springframework.web.reactive.function.client.WebClient.ResponseSpec;
 import reactor.core.publisher.Flux;
-import reactor.netty.http.client.HttpClient;
 
 @ExtendWith(MockitoExtension.class)
 class WeatherClientTest {
@@ -32,25 +24,17 @@ class WeatherClientTest {
     @Mock
     private WebClient webClient;
 
+    @Mock
+    private RequestHeadersUriSpec requestHeadersUriSpec;
+
+    @Mock
+    private RequestHeadersSpec requestHeadersSpec;
+
+    @Mock
+    private ResponseSpec responseSpec;
+
     @InjectMocks
     private WeatherClient weatherClient;
-
-    @BeforeEach
-    void setUp() {
-        webClient = WebClient.builder().baseUrl("https://f-api.github.io")
-                .clientConnector(new ReactorClientHttpConnector(
-                        HttpClient.create()
-                                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
-                                .responseTimeout(Duration.ofMillis(5000))
-                                .doOnConnected(connection ->
-                                        connection
-                                                .addHandlerLast(new ReadTimeoutHandler(5000, TimeUnit.MILLISECONDS))
-                                                .addHandlerLast(new WriteTimeoutHandler(5000, TimeUnit.MILLISECONDS))
-                                )
-                ))
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .build();
-    }
 
     @Test
     @DisplayName("날씨를 가져온다.")
@@ -58,13 +42,14 @@ class WeatherClientTest {
         // given
         final String date = "10-15";
 
-        final List<WeatherResponse> weatherResponses = List.of(
-                new WeatherResponse("10-14", "Sunny"),
-                new WeatherResponse("10-15", "Humid")
-        );
-
-        WebClient.ResponseSpec responseSpec = mock(WebClient.ResponseSpec.class);
-        when(responseSpec.bodyToFlux(WeatherResponse.class)).thenReturn(Flux.fromIterable(weatherResponses));
+        when(webClient.get()).thenReturn(requestHeadersUriSpec);
+        when(requestHeadersUriSpec.uri("/f-api/weather.json")).thenReturn(requestHeadersSpec);
+        when(requestHeadersSpec.retrieve()).thenReturn(responseSpec);
+        when(responseSpec.bodyToFlux(WeatherResponse.class))
+                .thenReturn(Flux.fromIterable(List.of(
+                        new WeatherResponse("10-14", "Sunny"),
+                        new WeatherResponse("10-15", "Humid")
+                )));
 
         // when
         final WeatherResponse response = weatherClient.getWeather(date);
